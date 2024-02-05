490ca3e054d2f90be06a6eba8d91776bc6f89ff9
erdembuke <erdembke@gmail.com>
2024-02-05 20:50:33 +0300

    webdriver ile ilk yonlendirme islemi yapildi

--- src/test/java/Day01/FirstProject.java
+++ src/test/java/Day01/FirstProject.java
@@ -1,8 +1,18 @@
 package Day01;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FirstProject {
 
+    public static void main(String[] args) {
 
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+
+    }
 
 
 }
