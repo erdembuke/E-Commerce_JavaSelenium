0ed720326b9fc3ee08e82c3bd8fa8b779eadb924
erdembuke <erdembke@gmail.com>
2024-02-06 23:42:19 +0300

    amazon sayfasi ilk testi POM kullanilarak tamamlandi

--- src/test/java/Pages/AmazonPage.java
+++ src/test/java/Pages/AmazonPage.java
@@ -1,7 +1,11 @@
 package Pages;
 
 import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class AmazonPage {
 
@@ -9,7 +13,29 @@ public class AmazonPage {
         PageFactory.initElements(GWD.getDriver(), this);
     }
 
+    @FindBy(id = "sp-cc-rejectall-link")
+    public WebElement rejectCookies;
 
+    @FindBy(id = "sp-cc-accept")
+    public WebElement acceptCookies;
+
+    @FindBy(id = "twotabsearchtextbox")
+    public WebElement searchBox;
+
+    @FindBy(id = "nav-search-submit-button")
+    public WebElement searchBtn;
+
+    @FindBy(css = "div[data-index=\"3\"] h2 span")
+    public WebElement firstItemAfterSearch;
+
+    @FindBy(css = "div[data-index=\"3\"] span[class=\"a-price-whole\"]")
+    public WebElement firstItemPricePt1;
+
+    @FindBy(css = "div[data-index=\"3\"] span[class=\"a-price-fraction\"]")
+    public WebElement firstItemPricePt2;
+
+    @FindBy(css = "span[class=\"a-size-base-plus a-color-base a-text-normal\"]")
+    public List<WebElement> productTitles;
 
 
 }
--- src/test/java/Tests/AmazonTest.java
+++ src/test/java/Tests/AmazonTest.java
@@ -1,4 +1,61 @@
 package Tests;
 
+import Pages.AmazonPage;
+import Utilities.ConfigReader;
+import Utilities.GWD;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class AmazonTest {
+    AmazonPage ap = new AmazonPage(); // for calling WebElements
+
+    @Test
+    public void searchProduct() throws InterruptedException {
+        // Navigate to Amazon
+        GWD.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        ap.rejectCookies.click();
+
+        // Type "Apple MacBook M3" in Search Box
+        ap.searchBox.sendKeys(ConfigReader.getProperty("textAmazon1"));
+        ap.searchBtn.click();
+        Thread.sleep(500);
+
+        // Assert the title contains searched product name
+        String title = GWD.getDriver().getTitle();
+        System.out.println("title -> " + title);
+        Assert.assertTrue(title.contains(ConfigReader.getProperty("textAmazon1")));
+
+        String firstItemText = ap.firstItemAfterSearch.getText();
+        System.out.println("Item: " + firstItemText);
+
+        String itemPrice = "";
+        itemPrice = ap.firstItemPricePt1.getText() + "," +
+                ap.firstItemPricePt2.getText() + " TL";
+        System.out.println("Price: " + itemPrice);
+
+        /*
+        Sayfanın ortasına scroll yapmak için:
+        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight/2)");
+         */
+
+        // Listing all search results
+        int count = 0;
+        for (WebElement titles : ap.productTitles) {
+            String titleTxt = titles.getText();
+            System.out.println("Item Title: " + titleTxt);
+            count++;
+            Assert.assertTrue(titleTxt.toLowerCase().contains("mac")); // every result contains mac
+        }
+
+        // Assertion of the search result total count
+        System.out.println("item count -> " + count);
+        Assert.assertEquals(count,ap.productTitles.size());
+
+
+        GWD.quitDriver();
+    }
+
+
 }
